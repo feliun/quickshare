a19d24c1a62593ec66969e9924c98994f807a4a6
feliun <felipe.polo@gmail.com>
2016-08-17 23:07:40 +0100

    Initial splash screen.WIP

---
+++ Quick Share/Quick Share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quick Share
+//
+//  Created by Gibbonsoft Limited on 17/08/2016.
+//  Copyright © 2016 feliun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
